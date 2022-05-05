dcc47b2ee3bf8f350945df863d6599d87e787458
zhengDaFeng <33956662+zhengDaFeng@users.noreply.github.com>
2022-05-05 14:29:28 +0800

    初始简陋工程

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp1/Model/AlarmModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace WpfApp1.Model
+{
+    public class AlarmModel : ObservableObject
+    {
+        private bool enable = true;
+        private string alarm = "18:00";
+        private string remain = "0小时0分钟内";
+        private string remark = "备注";
+        private ObservableCollection<bool> day = new ObservableCollection<bool>() { true, true, true, true, true, true, true };
+
+        public bool Enable
+        {
+            get => enable;
+            set => Set(ref enable, value);
+        }
+        public string Alarm
+        {
+            get => alarm;
+            set => Set(ref alarm, value);
+        }
+        public string Remain
+        {
+            get => remain;
+            set => Set(ref remain, value);
+        }
+        public string Remark
+        {
+            get => remark;
+            set => Set(ref remark, value);
+        }
+        public ObservableCollection<bool> DayOfWeekEnable
+        {
+            get => day;
+            set => Set(ref day, value);
+        }
+    }
+}
